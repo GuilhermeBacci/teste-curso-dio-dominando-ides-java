9e8d76a4d919371a9afff3b090863d1db966fa80
Glopes74 <guilhermebacci@alunos.utfpr.edu.br>
2021-10-20 11:22:01 -0300

    Criação de calculadora

---
+++ src/br/com/dio/calculadora/Calculadora.java
@@ -0,0 +1,42 @@
+package br.com.dio.calculadora;
+
+import java.util.Scanner;
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		int a, b;
+		
+		System.out.println("Digite o primeiro valor: ");
+		a = scan.nextInt();
+		System.out.println("Digite o segundo valor: ");
+		b = scan.nextInt();
+		
+		int soma = soma(a,b);
+		int subtracao = subtracao(a,b);
+		int mutiplicacao = mutiplicacao(a,b);
+		int divisao = divisao(a,b);
+		
+		System.out.println("soma " + soma);
+		System.out.println("subtracao " + subtracao);
+		System.out.println("mutiplicacao " + mutiplicacao);
+		System.out.println("divisao " + divisao);
+		
+	}
+	public static int soma(int a, int b) {
+		return a + b;
+	}
+	public static int subtracao(int a, int b) {
+		return a - b;
+	}
+	public static int divisao(int a, int b) {
+		return a / b;
+	}
+	public static int mutiplicacao(int a, int b) {
+		return a * b;
+	}
+	
+	
+}
